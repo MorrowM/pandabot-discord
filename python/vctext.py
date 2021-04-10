e2a71860e7e687c157509c3489a292d4c1897fc8
Morrow <themorrowm@gmail.com>
2021-04-10 12:18:17 +0300

    added join and leave messages to the vctext script

--- python/vctext.py
+++ python/vctext.py
@@ -18,19 +18,25 @@ async def on_ready():
 @client.event
 async def on_voice_state_update(member, before, after):
     for vctext_role in cfg['voiceConfig']['roles']:
-        if not (before.channel and before.channel.id in vctext_role['channels']) and \
-                (after.channel and after.channel.id in vctext_role['channels']):
+        if not (before.channel and before.channel.id in vctext_role['voiceChannels']) and \
+                (after.channel and after.channel.id in vctext_role['voiceChannels']):
             await member.add_roles(Object(vctext_role['role']),
                                    reason=f'User joined {vctext_role["name"]}')
             print(
                 f'Added the {vctext_role["name"]} vctext role to {member.name}')
 
-        if (before.channel and before.channel.id in vctext_role['channels']) and \
-                not (after.channel and after.channel.id in vctext_role['channels']):
+            text_channel = vctext_role.get('textChannel')
+            if text_channel is not None:
+                await client.get_channel(text_channel).send(f'{member.name} joined {vctext_role["name"]}')
+        if (before.channel and before.channel.id in vctext_role['voiceChannels']) and \
+                not (after.channel and after.channel.id in vctext_role['voiceChannels']):
             await member.remove_roles(Object(vctext_role['role']),
                                       reason=f'User left {vctext_role["name"]}')
             print(
                 f'Removed the {vctext_role["name"]} vctext role from {member.name}')
 
+            text_channel = vctext_role.get('textChannel')
+            if text_channel is not None:
+                await client.get_channel(text_channel).send(f'{member.name} left {vctext_role["name"]}')
 
 client.run(BOT_TOKEN)
