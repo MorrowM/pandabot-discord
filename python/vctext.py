cf84bdecb5e095e5cca630fb1d332124c00d1b83
Morrow <themorrowm@gmail.com>
2021-04-10 13:59:49 +0300

    make names bold in vctext messages

--- python/vctext.py
+++ python/vctext.py
@@ -27,7 +27,7 @@ async def on_voice_state_update(member, before, after):
 
             text_channel = vctext_role.get('textChannel')
             if text_channel is not None:
-                await client.get_channel(text_channel).send(f'{member.name} joined the channel')
+                await client.get_channel(text_channel).send(f'**{member.name}** joined the channel')
 
         if (before.channel and before.channel.id in vctext_role['voiceChannels']) and \
                 not (after.channel and after.channel.id in vctext_role['voiceChannels']):
@@ -38,6 +38,6 @@ async def on_voice_state_update(member, before, after):
 
             text_channel = vctext_role.get('textChannel')
             if text_channel is not None:
-                await client.get_channel(text_channel).send(f'{member.name} left the channel')
+                await client.get_channel(text_channel).send(f'**{member.name}** left the channel')
 
 client.run(BOT_TOKEN)
