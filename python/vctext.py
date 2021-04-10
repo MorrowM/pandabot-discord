79e16c6aac9b05b92e68ecc4e124f265670d3b5d
Morrow <themorrowm@gmail.com>
2021-04-10 12:26:21 +0300

    changed join and leave messages

--- python/vctext.py
+++ python/vctext.py
@@ -27,7 +27,8 @@ async def on_voice_state_update(member, before, after):
 
             text_channel = vctext_role.get('textChannel')
             if text_channel is not None:
-                await client.get_channel(text_channel).send(f'{member.name} joined {vctext_role["name"]}')
+                await client.get_channel(text_channel).send(f'{member.name} joined the channel')
+
         if (before.channel and before.channel.id in vctext_role['voiceChannels']) and \
                 not (after.channel and after.channel.id in vctext_role['voiceChannels']):
             await member.remove_roles(Object(vctext_role['role']),
@@ -37,6 +38,6 @@ async def on_voice_state_update(member, before, after):
 
             text_channel = vctext_role.get('textChannel')
             if text_channel is not None:
-                await client.get_channel(text_channel).send(f'{member.name} left {vctext_role["name"]}')
+                await client.get_channel(text_channel).send(f'{member.name} left the channel')
 
 client.run(BOT_TOKEN)
